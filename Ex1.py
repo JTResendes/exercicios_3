62badf077589bc304fc4e2f78095aa35917ee6a9
JoaoResendes <93650121+JoaoResendes@users.noreply.github.com>
2022-09-26 17:00:37 +0100

    ex1

--- Ex1.py
+++ Ex1.py
@@ -8,3 +8,14 @@ Após o utilizador ter inserido os 5 valores apresente:
 - O maior valor inserido
 - A média das vendas
 """
+
+listaVendas = [100, 50, 200, 300, 400]
+listailhas = ['Terceira', 'Pico', 'Faial', 'São Jorge', 'Graciosa']
+
+soma = sum(listaVendas)
+minimo = min(listaVendas)
+maximo = max(listaVendas)
+print(f'Mínimo Valor {minimo}')
+print(f'Máximo Valor {maximo}')
+print(f'Total de Vendas {soma}')
+
